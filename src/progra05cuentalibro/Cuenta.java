9674ec3f059d07121fd986477156c299f8f67953
Jonathan <jonathan.sotero30@gmail.com>
2022-05-16 16:16:18 -0500

    Primera version

---
+++ src/progra05cuentalibro/Cuenta.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra05cuentalibro;
+
+/**
+ *
+ * @author HP
+ */
+public class Cuenta {
+    
+    private String nombre;
+    
+    public void establecerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String obtenerNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+}
+    
+
---
+++ src/progra05cuentalibro/Progra05CuentaLibro.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra05cuentalibro;
+
+/**
+ *
+ * @author HP
+ */
+public class Progra05CuentaLibro {
+
+    /**
+     * @param args the command line arguments
+     *
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Cuenta cuenta001 = new Cuenta();
+        
+        Cuenta cuenta002 = new Cuenta();
+        cuenta002.setNombre("Jonathan");
+        
+        System.out.println(cuenta002.getNombre());
+    }
+
+}
+    
+        
+        
+       
+      
+    
+        
+        
+        
+        
+    
+
+
+    
+    
+
+    
+
+    
+    
+
+    
+
+     
